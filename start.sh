d6afa45b714ab63560011a7a21e316c342fcefd0
York Tang <ivanstang0415@gmail.com>
2019-03-04 23:12:40 +0800

    fix bug

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/ivanstang/ssr-config/master/deploy_host.sh | bash
+if [ -f "deploy_host.sh"]; then
+	rm "deploy_host.sh"
+fi
+
+wget https://raw.githubusercontent.com/ivanstang/ssr-config/master/deploy_host.sh
+bash deploy_host.sh
